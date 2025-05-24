4e97136871dab20537e0dfb649aca6fd9daed2cb
DESKTOP-9FTBJNR\Admin <nguyentthanh26091997@gmail.com>
2025-05-24 13:01:16 +0700

    update: remove queue.c, move logic to queue.h

--- Components/core/queue.h
+++ Components/core/queue.h
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stddef.h>
+#include <assert.h>
 
 /**
  * @brief Structure representing a circular queue.
@@ -29,7 +30,16 @@ typedef struct Queue
  * @param buf Pointer to the buffer to be used for the queue.
  * @param size Size of the buffer in bytes.
  */
-void Queue_Init(queue_t* q, uint8_t* buf, uint16_t size);
+static inline void Queue_Init(queue_t* q, uint8_t* buf, uint16_t size)
+{
+	assert(q != NULL);
+
+	q->first = buf;
+	q->size = size;
+	q->last = q->first + size;
+	q->inPtr = q->first;
+	q->outPtr = q->first;
+}
 
 /**
  * @brief Resets a circular queue.
@@ -38,7 +48,13 @@ void Queue_Init(queue_t* q, uint8_t* buf, uint16_t size);
  *
  * @param q Pointer to the queue structure to be reset.
  */
-void Queue_Reset(queue_t* q);
+static inline void Queue_Reset(queue_t* q)
+{
+	assert(q != NULL);
+
+	q->inPtr = q->first;
+	q->outPtr = q->first;
+}
 
 /**
  * @brief Checks the number of free bytes in the queue.
@@ -48,7 +64,44 @@ void Queue_Reset(queue_t* q);
  * @param q Pointer to the queue structure.
  * @return The number of free bytes in the queue.
  */
-uint16_t Queue_CheckNumOfFree(queue_t* q);
+static inline uint16_t Queue_CheckNumOfFree(queue_t* q)
+{
+	assert(q != NULL);
+
+	uint16_t ret = q->size + q->outPtr - q->inPtr;
+	if (ret > q->size) ret -= q->size;
+	return (ret - 1);
+}
+
+/**
+ * @brief Checks if the queue is empty.
+ *
+ * This function determines whether the queue contains any elements.
+ *
+ * @param q Pointer to the queue structure.
+ * @return `true` if the queue is empty, `false` otherwise.
+ */
+static inline bool Queue_IsEmpty(queue_t* q)
+{
+	assert(q != NULL);
+
+	return (q->inPtr == q->outPtr);
+}
+
+/**
+ * @brief Checks if the queue is full.
+ *
+ * This function determines whether the queue has reached its maximum capacity.
+ *
+ * @param q Pointer to the queue structure.
+ * @return `true` if the queue is full, `false` otherwise.
+ */
+static inline bool Queue_IsFull(queue_t* q)
+{
+	assert(q != NULL);
+
+	return ((q->inPtr + 1 == q->outPtr) || (q->inPtr == q->last - 1 && q->outPtr == q->first));
+}
 
 /**
  * @brief Removes an element from the queue.
@@ -59,7 +112,20 @@ uint16_t Queue_CheckNumOfFree(queue_t* q);
  * @param val Pointer to a variable where the removed value will be stored.
  * @return `true` if an element was successfully removed, `false` if the queue is empty.
  */
-bool Queue_Pop(queue_t* q, uint8_t* val);
+static inline bool Queue_Pop(queue_t* q, uint8_t* val)
+{
+	assert(q != NULL);
+	assert(val != NULL);
+
+	if (q->outPtr != q->inPtr)
+	{
+		*val = *(q->outPtr);
+		q->outPtr++;
+		if (q->outPtr == q->last) q->outPtr = q->first;
+		return true;
+	}
+    return false;
+}
 
 /**
  * @brief Adds an element to the queue.
@@ -70,26 +136,19 @@ bool Queue_Pop(queue_t* q, uint8_t* val);
  * @param val The value to be added to the queue.
  * @return `true` if the element was successfully added, `false` if the queue is full.
  */
-bool Queue_Push(queue_t* q, uint8_t val);
+static inline bool Queue_Push(queue_t* q, uint8_t val)
+{
+	assert(q != NULL);
 
-/**
- * @brief Checks if the queue is empty.
- *
- * This function determines whether the queue contains any elements.
- *
- * @param q Pointer to the queue structure.
- * @return `true` if the queue is empty, `false` otherwise.
- */
-bool Queue_IsEmpty(queue_t* q);
-
-/**
- * @brief Checks if the queue is full.
- *
- * This function determines whether the queue has reached its maximum capacity.
- *
- * @param q Pointer to the queue structure.
- * @return `true` if the queue is full, `false` otherwise.
- */
-bool Queue_IsFull(queue_t* q);
+	uint8_t* next = q->inPtr + 1;
+	if (next == q->last) next = q->first;
+	if (next != q->outPtr)
+	{
+		*(q->inPtr) = val;
+		q->inPtr = next;
+		return true;
+	}
+	else return false;
+}
 
 #endif /* CORE_QUEUE_H_ */
